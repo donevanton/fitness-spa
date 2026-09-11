c72b17816d2bb06703b869687ea0a22914367415
donevanton <donevanton@gmail.com>
2026-09-11 12:25:56 +0300

    index

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/fitness-spa/",
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/fitness-spa/",
->>>>>>> 131c1bd (Initial fitness app)
 });
